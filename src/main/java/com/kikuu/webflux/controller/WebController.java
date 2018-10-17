b376e40b5851b702808a96e2a03c64fd5f984a3c
blithe <weihang.xu@kikuu.com>
2018-10-17 17:52:51 +0800

    111

---
+++ src/main/java/com/kikuu/webflux/controller/WebController.java
@@ -0,0 +1,20 @@
+package com.kikuu.webflux.controller;
+
+/**
+ * @author Jed
+ */
+public class WebController {
+//    @Autowired
+//    private BannerHandler bannerHandler;
+//
+//    @Bean
+//    public RouterFunction<ServerResponse> webFluxRoutesRegister(){
+//        return RouterFunctions
+//                .route(RequestPredicates.GET("/banner/getList"), bannerHandler::getList)//查询 json响应
+//                .andRoute(RequestPredicates.PUT("/banner/update"), bannerHandler::update)//更新 json响应
+//                .andRoute(RequestPredicates.DELETE("/banner/deletes/{ids}"), bannerHandler::deletes)//删除 json响应
+//                .andRoute(RequestPredicates.GET("/banner/init"), bannerHandler::init)//页面渲染 html响应
+//                .andRoute(RequestPredicates.POST("/banner/upload"), bannerHandler::upload)//文件上传 json响应
+//                .andRoute(RequestPredicates.GET("/banner/download"), bannerHandler::download);//文件下载 stream IO
+//    }
+}
